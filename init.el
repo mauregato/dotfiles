90b5f790aedf06f19c6907edf1abe715832b3aef
mauregato <hideo03@gmail.com>
2019-05-23 13:35:03 +0200

    .

--- init.el
+++ init.el
@@ -1,1 +1,4 @@
-(setq py-python-command "python3")
+;;(setq py-python-command "python3")
+(setq python-shell-interpreter "python3")
+(setq python-indent-guess-indent-offset t)
+(setq python-indent-guess-indent-offset-verbose nil)
